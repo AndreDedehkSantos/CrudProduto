69c8dcb73118366702cba24bdf69f53e0893ccb2
AndreDedehkSantos <andreluis.cosme@hotmail.com>
2020-06-28 16:35:14 -0300

    Primeira classe AcessorioBasico

---
+++ CrudProduto/Models/AcessorioBasico.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudProduto.Models
+{
+	public class AcessorioBasico
+	{
+		public int id { get; set; }
+		public  string nome{ get; set; }
+		public string descricao { get; set; }
+		public int quantidade { get; set; }
+	}
+}
